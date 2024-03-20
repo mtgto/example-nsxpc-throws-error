80b809376474af746c968afc39491c189198be77
mtgto <hogerappa@gmail.com>
2024-03-20 18:17:50 +0900

    Initial Commit

---
+++ example-nsxpc-throws-error/ContentView.swift
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2024 mtgto <hogerappa@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ example-nsxpc-throws-error/example_nsxpc_throws_errorApp.swift
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 mtgto <hogerappa@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+@main
+struct example_nsxpc_throws_errorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
